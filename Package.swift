49a6678407432c5c5af8ea9ef874f0cd5d4466f9
JPMartha <jpmartha.jp@gmail.com>
2016-03-27 08:48:32 +0900

    Fix Travis CI Support

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "APIHimotoKit",
     dependencies: [
+        .Package(url: "https://github.com/JPMartha/APIKit.git",
+            Version(2, 0, 0)),
         .Package(url: "https://github.com/JPMartha/Himotoki.git",
             Version(2, 0, 0))
     ],
