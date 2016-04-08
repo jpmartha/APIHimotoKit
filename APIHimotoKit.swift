8a6c10b0ac480f5b8dc8a9de24ac79e22b4db122
JPMartha <jpmartha.jp@gmail.com>
2016-04-08 10:35:40 +0900

    Improve .travis.yml

---
+++ APIHimotoKit.swift
@@ -0,0 +1,15 @@
+//
+//  APIHimotoKit.swift
+//  APIHimotoKit
+//
+//  Created by JPMartha on 2016/03/27.
+//
+//
+
+import Foundation
+
+class APIHimotoKit {
+    func test() {
+        print("Hello, world!!")
+    }
+}
